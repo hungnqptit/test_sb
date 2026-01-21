d115ca18d97f20e47a6a2a6f8cbd7216de60bc12
hungnqptit <49617472+hungnqptit@users.noreply.github.com>
2026-01-21 16:30:04 +0700

    Implement API for release patches retrieval
    Add endpoint to retrieve release patches with artifacts

--- index.js
+++ index.js
@@ -80,3 +80,26 @@ app.get("/api/v1/apps/457789c6-f756-4650-a3e1-06da8d2032fc/releases/1/artifacts"
     }
   ] });
 });
+app.get("/api/v1/apps/457789c6-f756-4650-a3e1-06da8d2032fc/releases/1/patches", (req, res) => {
+  console.log("== release patches request", req);
+  res.send({ artifacts: [
+    {
+       id: "1",
+        number: "1",
+    channel: "",
+     artifacts:[   {
+      id: 0,
+      release_id: 1,
+      arch: "aarch64",
+      platform: "android",
+      url: "https://github.com/hungnqptit/test_sb/releases/download/bundle/app-release.aab",
+      hash: "44575fb35dde8a0521f9cd3b7fb66536d3518294",
+      size: 42,
+      podfile_lock_hash: null,
+      can_sideload: true
+    }],
+    isRolledBack: false,
+    notes: "test",
+    }
+  ] });
+});
