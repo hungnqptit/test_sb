7343867428c470266e6a19579d5dfe4773e10a7b
hungnqptit <49617472+hungnqptit@users.noreply.github.com>
2026-01-21 16:31:24 +0700

    Change response key from 'artifacts' to 'patches'

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get("/api/v1/apps/457789c6-f756-4650-a3e1-06da8d2032fc/releases/1/artifacts"
 });
 app.get("/api/v1/apps/457789c6-f756-4650-a3e1-06da8d2032fc/releases/1/patches", (req, res) => {
   console.log("== release patches request", req);
-  res.send({ artifacts: [
+  res.send({ patches: [
     {
        id: "1",
         number: "1",
